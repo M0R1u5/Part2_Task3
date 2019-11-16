12d6be0473d267ac7d5964c42614166e0b0ff6e8
M0r1u5 <kuki.amakuki@gmail.com>
2019-11-16 15:48:55 +0600

    1

---
+++ Task3.py
@@ -0,0 +1,4 @@
+number = int(input('Enter any number: '))
+
+print('The next number for ' + str(number) + ' is ' + str(number+1))
+print('The previous number for ' + str(number) + ' is ' + str(number-1))
